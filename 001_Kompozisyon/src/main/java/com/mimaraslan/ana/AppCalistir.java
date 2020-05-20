f7504a2c9f26b8a8030c436851749ee2d1280dab
taner <taner@MSI>
2020-05-20 23:11:05 +0200

    Spring Kompozisyon Project

---
+++ 001_Kompozisyon/src/main/java/com/mimaraslan/ana/AppCalistir.java
@@ -0,0 +1,13 @@
+package com.mimaraslan.ana;
+
+public class AppCalistir {
+
+	public static void main(String[] args) {
+		
+		Araba araba = new Araba();
+		araba.hareketeGec();
+		
+
+	}
+
+}
---
+++ 001_Kompozisyon/src/main/java/com/mimaraslan/ana/Araba.java
@@ -0,0 +1,17 @@
+package com.mimaraslan.ana;
+
+public class Araba {
+	
+	private Yakit yakit;
+	
+	public Araba() {
+		
+		this.yakit=new Yakit();
+	}
+	
+	public void hareketeGec() {
+		yakit.depo();
+		System.out.println("Araba Harekete Gecti");
+	}
+
+}
